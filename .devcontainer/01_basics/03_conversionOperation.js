2fcd14dcdf2e347fe57a857fdc07721a42186cce
shailendraenthusiastics <sky04062005@gmail.com>
2024-10-13 05:32:52 +0000

    comparision in javascript

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -19,3 +19,16 @@ let someNumber=33
 let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *********************** Operations ******************************************************
+let value=3
+let negValue=-value;
+//console.log(negValue)
+let str1="hello"
+let str2="  Shailendra"
+let str3=str1 + str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+
