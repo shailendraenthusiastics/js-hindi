5584219247eef1bc1a9e204be315e9407d800020
shailendraenthusiastics <sky04062005@gmail.com>
2024-10-15 14:38:48 +0000

    object_2_injs

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,40 @@
+//const tinderUser = new Object();  // singleton object
+const tinderUser ={}
+
+tinderUser.id="123abc"
+tinderUser.name ="Shailendra"
+tinderUser.isLoggedIn=false
+
+//console.log(tinderUser);
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Shailendra",
+            lastname:"Kumar"
+        }
+    }
+}
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={
+    1:"a",
+    2:"b"
+
+}
+const obj2={
+    3:"a",4:"b"
+}
+const obj4 ={5:"a",6:"b"}
+//const obj3 ={obj1,obj2}
+//const obj3 = Object.assign({},obj1,obj2,obj4)
+const obj3 ={...obj1, ...obj2}
+//console.log(obj3)
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
