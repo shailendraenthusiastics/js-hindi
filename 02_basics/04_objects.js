723ce6f9255a290da6966db6024bfc0f70fbfe5c
shailendraenthusiastics <sky04062005@gmail.com>
2024-10-16 17:18:11 +0000

    Json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -32,9 +32,26 @@ const obj4 ={5:"a",6:"b"}
 const obj3 ={...obj1, ...obj2}
 //console.log(obj3)
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:" hitesh "
+}
+//course.courseInstructor
+const {courseInstructor:instructor} =course   // de structuring of object
+console.log(instructor);
+
+// {
+//     "name":"Shailendra",
+//     "coursename":"js in hindi",
+//     "price":"free"
+
+// }  json syntax
